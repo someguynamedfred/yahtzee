7f01f78162e9911de70c41c2bc8af34435fece6d
someguynamedfred <134651167+someguynamedfred@users.noreply.github.com>
2024-01-24 11:56:41 -0500

    Update yahtzee.py
    cleaned up/removed some elements and converted some code into comments.

--- yahtzee.py
+++ yahtzee.py
@@ -1,13 +1,7 @@
 #stage one of my yahtzee game
 import random
-
-#this is the basic dice roll
-cont = input("Do you want to continue?")
-if cont == "yes" or cont == "y":
-    dice = random.randint(1,6)
-    print(dice)
     
-# check if there are enough elements and assign random numbers to the list of dice
+# check if there are enough elements and assign random numbers to the list of dice. This is essentially the start of turn. 
 import random
 length = False
 test_list = []
@@ -20,17 +14,15 @@ while length == False:
         length = True
 
 print(test_list)
-
-print(test_list)
     
 #make a scoring system
 #if the kept dice equal a certain list of integers, then a certain score is awarded
 
 #large_straight is worth 40 points
-large_straight = [1, 2, 3, 4, 5] or [2, 3, 4, 5, 6]
+#large_straight = [1, 2, 3, 4, 5] or [2, 3, 4, 5, 6]
 
 #small_straight is worth 30 points
-small_straight = [1, 2, 3, 4] or [2, 3, 4, 5] or [3, 4, 5, 6]
+#small_straight = [1, 2, 3, 4] or [2, 3, 4, 5] or [3, 4, 5, 6]
 
 #3 of a kind has three matching elements and it is worth sum of dice
 
@@ -41,6 +33,3 @@ small_straight = [1, 2, 3, 4] or [2, 3, 4, 5] or [3, 4, 5, 6]
 #chance equals total value of all dice
 
 #yahtzee is 50 points for first success and then 100 for each after that
-
-test_list = [1, 2, 3, 5]
-print(len(test_list))
